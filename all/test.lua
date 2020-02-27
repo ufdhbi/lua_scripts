30a60cda36d3f86591708a1eae6b76765c230d6e
ufdhbi <lacyer1@gmail.com>
2020-02-28 02:33:11 +0300

    Update test.lua

--- all/test.lua
+++ all/test.lua
@@ -2,11 +2,13 @@ local sampev = require "samp.events"
 local encoding = require 'encoding'
 encoding.default = 'CP1251'
 local u8 = encoding.UTF8
+local function u8d(s) return u8:decode(s) end
+
 
 function main()
     if not isSampfuncsLoaded() or not isSampLoaded() then return end
     while not isSampAvailable() do wait(100) end
-    sampAddChatMessage(u8:decode("Тест кодировки"), -100)
+    sampAddChatMessage(u8d"Тест кодировки", -100)
     while true do
         wait(0)
         
